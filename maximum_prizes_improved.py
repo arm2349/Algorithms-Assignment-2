4c1d4b37d4dd06850820e4802942deeadef1635b
arm2349 <56358399+arm2349@users.noreply.github.com>
2020-07-15 14:04:37 -0400

    Update maximum_prizes_improved.py

--- maximum_prizes_improved.py
+++ maximum_prizes_improved.py
@@ -13,21 +13,23 @@ def optimal_summands(n):
 
 
     while remainder>0:
+        #add i to the list, as long as it is not already in it. To check if i is in the list, it is only necessary to inspect the last element.
         if lst_of_summands[-1] != i:
             lst_of_summands.append(i)
             remainder-=i
             i+=1
+            
+            #if i+i equals the remainder, then you can no longer use i, since you would have to add it twice and our list of summands can only contain distinct integers.
+            #this means that the remainder cannot be broken down any further, so just add the remainder itself to the list.
             if (i*2)==remainder:
                 lst_of_summands.append(remainder)
+                # "use up" the remainder, and break out of the cycle as there are no remainding summands to append to the list.
                 remainder=0
                 break
-            #added conditions !=0 because 0 is in the list by default.
             while 0 < (remainder - (i)) <i:
                 i+=1
+    #remove 0 from the list, since we are going to print the length of the list in order to display the number of summands (0 is not a summand).
     del lst_of_summands[0]
-
-
-
 
 
 
