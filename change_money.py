75d5e847a58a9cff591f4de237ba45961ededffe
arm2349 <56358399+arm2349@users.noreply.github.com>
2020-07-20 15:56:56 -0400

    Update change_money.py
    -use divmod instead of subtraction

--- change_money.py
+++ change_money.py
@@ -1,12 +1,13 @@
 def change_money(m):
-    assert m>=1 and m<=1000
+    assert 1<= m<=1000
     amount_given=m
     coins=[10,5,1]
     minimum_coins=0
     for i in range(len(coins)):
-        while amount_given>=coins[i]:
-            minimum_coins+=1
-            amount_given-=coins[i]
+        if amount_given==0:
+            break
+        coin=coins[i]
+        quotient_and_remainder=divmod(amount_given, coin)
+        minimum_coins+=quotient_and_remainder[0]
+        amount_given=quotient_and_remainder[1]
     return minimum_coins
-inp=int(input())
-print (change_money(inp))
