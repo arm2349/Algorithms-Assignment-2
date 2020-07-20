f0109877415febf6fdd7f9f82aa5b8c5e7016937
arm2349 <56358399+arm2349@users.noreply.github.com>
2020-07-20 15:40:11 -0400

    Update maximum_revenue.py
    -simplified assert statement on line 8
    -removed Reverse parameter for sort function as it served no purpose

--- maximum_revenue.py
+++ maximum_revenue.py
@@ -4,14 +4,14 @@ import sys
 def max_dot_product(a, b):
     #list 'a' contains the profit-per-click
     #list 'b' contains the expected number of clicks per day
-    #return dot product of a and b sorted from highest to lowest to obtain maximum revenue
-    assert n>=1 and n<=1000
+    #return dot product of a and b sorted to obtain maximum revenue
+    assert 1<=n<=1000
     for i in a:
         assert i>=(-10)**5
     for i in b:
         assert i<=10**5
-    a.sort(reverse=True)
-    b.sort(reverse=True)
+    a.sort()
+    b.sort()
     revenue = 0
     for i in range(len(a)):
         revenue += a[i] * b[i]
